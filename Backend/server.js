8911abd75ed31d3a63d9e72395256a9215684c71
thatbulbul <bulbulsharma3363@gmail.com>
2026-05-01 02:44:36 +0530

    Update: The Api is working, able to request and get response

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,39 @@
-import OpenAI from 'openai';
+import express from "express";
 import "dotenv/config";
+import cors from "cors";
+import OpenAI from "openai";
 
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+const client=new OpenAI({
+  apiKey:process.env.OPENAI_API_KEY
 });
 
-const response = await client.responses.create({
-  model: 'gpt-4o-mini',
-  input: 'Joke related to computer science',
-});
+app.get("/test", async (req, res) => {
+  try {
+    const response = await client.responses.create({
+      model: "gpt-4.1-mini",
+      input: [
+        {
+          role: "user",
+          content: req.body.message
+        }
+      ]
+    });
 
-console.log(response.output_text);
+    const reply= response.output[0].content[0].text;
+    res.send(reply);
+     } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
+});
+  
+
+app.listen(PORT, () => {
+  console.log(`server runnng on ${PORT}`);
+});
